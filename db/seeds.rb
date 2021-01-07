d87240ac3810fd07e7e63075a1e22356bde3856c
prashant-shekhar <toprashantshekhar@gmail.com>
2021-01-08 02:01:44 +0530

    Integrated Index action in portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam iaculis massa quis ipsum imperdiet porta. Fusce id porttitor ligula. Aenean quis purus turpis. Cras pulvinar ante sed pellentesque dapibus. Nulla sollicitudin, enim ac lacinia sollicitudin, libero massa pretium justo, sed ultricies risus nisl vel metus. Ut pellentesque, elit vitae euismod porttitor, lorem sem tempor velit, quis ullamcorper mi ipsum quis augue. Fusce non aliquam mauris."
+  )
+end
+
+puts "10 blog post created"
+
+5.times do |_skills|
+  Skill.create!(
+    title: "Rails",
+    percent_utilized: 30
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_items|
+  Portfolio.create!(
+    title: "Portfolio_title #{portfolio_items}",
+    subtitle: "My great service",
+    body: "Quisque ut eleifend ipsum, ac lobortis ligula. Fusce eget quam libero. Pellentesque nec sapien aliquet, pellentesque est id, luctus sem. Vivamus nec elit consequat, scelerisque ex id, rhoncus risus. Ut tincidunt scelerisque porta. Etiam cursus sem sapien, quis sodales elit accumsan quis. Praesent vitae purus blandit quam commodo tristique vel ut augue. Duis in tristique augue, id tempus nisl. Nulla blandit dui sed tristique tempor. Curabitur varius lacus quis nulla porttitor, ut laoreet ante congue. Sed sed volutpat augue. Maecenas congue, orci quis volutpat tincidunt, massa elit laoreet eros, a euismod diam dolor a lacus. Suspendisse ultrices turpis eu aliquet pretium. Nullam ac dictum enim, ac tincidunt urna. Nulla facilisi.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/300x250"
+
+  )
+end
+puts "9 portfolio items created"
