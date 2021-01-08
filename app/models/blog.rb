4e07b9179258c729e74a3ee5a4a09eff876ee71a
prashant-shekhar <toprashantshekhar@gmail.com>
2021-01-08 15:06:26 +0530

    Added Enum in the Blog section

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,5 @@
 class Blog < ApplicationRecord
+  enum status: { draft: 0, published: 1 }  # attribute of enum i.e. status
   extend ::FriendlyId
   friendly_id :title, use: :slugged
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_073127) do
+ActiveRecord::Schema.define(version: 2021_01_08_081225) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_01_08_073127) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
