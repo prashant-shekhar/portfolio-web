32a6188e39233e6aadbc6a60aaa6f88dd5193573
prashant-shekhar <toprashantshekhar@gmail.com>
2021-01-09 02:52:17 +0530

    Added data Defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,4 +4,11 @@ class Portfolio < ApplicationRecord
   def self.angular
     where(subtitle: "Angular")
   end
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/300x250"
+  end
 end
