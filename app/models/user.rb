650126a78b8e70c5aa11f852fa25c881d425d02c
prashant-shekhar <toprashantshekhar@gmail.com>
2021-01-10 13:26:32 +0530

    Completed aythentication with virtual attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :name
+  def first_name
+    name.split.first
+  end
+
+  def last_name
+    name.split.last
+  end
 end
