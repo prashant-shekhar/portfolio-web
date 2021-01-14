f5e308e5ec7c6502ef577995001866b58c079f77
prashant-shekhar <toprashantshekhar@gmail.com>
2021-01-14 17:48:31 +0530

    Implemented Authorization Engine

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,17 @@
 module ApplicationHelper
-  def sample_helper
-    "<p> My helper </p>".html_safe
+  def login_helper
+    if current_user.is_a?(GuestUser)
+      (link_to "Register", new_user_registration_path) +
+        "<br>".html_safe + (link_to "Login", new_user_session_path)
+    else
+      link_to "Logout", destroy_user_session_path, method: :delete
+    end
+  end
+end
+
+def source_helper(layout_name)
+  if session[:source]
+    greeting = "Thanks for visiong me from #{session[:source]} and you are on the #{layout_name} layout"
+    content_tag(:p, greeting, class: "source-greeting")
   end
 end
